35263db76559db05444f65cc4c882dca16206843
BrianCompau <brcompau@gmail.com>
2024-11-12 15:42:36 -0500

    changes for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Page1 from './Page1'
-import Page2 from './Page2'
 
 const App: React.FC = () => (
 	<BrowserRouter>
 		<Routes>
-			<Route path="/" element={<Page1 />} />
-			<Route path="users/" element={<Page2 />} />
+			<Route path="/bf-demo/" element={<Page1 />} />
 		</Routes>
 	</BrowserRouter>
 );
